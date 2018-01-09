27438b470bc64e3bd2bc598a34c806d948114008
PancakeBeast <johannes.botten@hotmail.no>
2018-01-09 10:00:24 +0100

    ll

--- risk/risk.js
+++ risk/risk.js
@@ -1,31 +1,46 @@
 function setup() {
-    var config = {
-        apiKey: "AIzaSyBlPjI5glXQ6ilEg9EVVPQS1lFh_Bsfa7c",
-        authDomain: "risk-b1fc9.firebaseapp.com",
-        databaseURL: "https://risk-b1fc9.firebaseio.com",
-        projectId: "risk-b1fc9",
-        storageBucket: "",
-        messagingSenderId: "217937368827"
-      };
-      firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyDK7BYlg-m6kCjE41ux_BNNX7FVCKzrbNw",
+    authDomain: "risk-3f9e9.firebaseapp.com",
+    databaseURL: "https://risk-3f9e9.firebaseio.com",
+    projectId: "risk-3f9e9",
+    storageBucket: "risk-3f9e9.appspot.com",
+    messagingSenderId: "492153867231"
+  };
+  firebase.initializeApp(config);
 
-      let database = firebase.database();
+  let database = firebase.database();
 
-      let land = database.ref("land");
-      land.on("child_added", visLand)
+  let land = database.ref("land");
+  land.on("child_added", visLand)
 
-      let kort = database.ref("kort");
-      kort.on("child_added", visKort)
+  let kort = database.ref("kort");
+  kort.on("child_added", visKort)
+
+  let spiller = database.ref("spiller");
+  spiller.on("child_added", visSpiller)
 }
 
 function visLand(snapshot) {
-    let land = snapshot.key;
-    let divTarget = document.getElementById("main");
-    divTarget.innerHTML += `<div>${land}</div>`
+  let land = snapshot.key;
+  let divTarget = document.getElementById("land");
+  divTarget.innerHTML += `<div class="box">${land}</div>`
 }
 
 function visKort(snapshot) {
   let kort = snapshot.key;
-  let divTarget = document.getElementById("main");
+  let divTarget = document.getElementById("kort");
   divTarget.innerHTML += `<div>${kort}</div>`
+}
+
+function visSpiller(snapshot) {
+  let spiller = snapshot.val();
+  let divTarget = document.getElementById("spiller");
+  divTarget.innerHTML += 
+  `<div>   
+<br>Navn ${spiller.navn}
+<br>Farge ${spiller.farge}
+<br>Alder ${spiller.alder}
+${spiller}
+  </div>`
 }
